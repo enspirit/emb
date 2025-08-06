a445bb9188606c5f46afb4245c6484e66c16ef99
Louis Lambeau <louis.lambeau@enspirit.be>
2025-08-06 12:30:01 +0200

    Config loading.

--- src/cli/hooks/init.ts
+++ src/cli/hooks/init.ts
@@ -1,7 +1,13 @@
 import { Hook } from '@oclif/core';
 
+import { loadConfig } from '../../config/index.js';
+
 const hook: Hook.Init = async function (options) {
-  console.log(`example init hook running before ${options.id}`);
+  try {
+    await loadConfig();
+  } catch (error) {
+    options.context.error(error as Error);
+  }
 };
 
 export default hook;
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,39 @@
+import { findUp } from 'find-up';
+import yaml from 'yaml';
+
 export type Component = {
   name: string;
 };
 
 export type Service = {
   name: string;
+};
+
+export type Config = {
+  project: { name: string };
+};
+
+let config: Config;
+export const loadConfig = async (force = false) => {
+  if (config && !force) {
+    return config;
+  }
+
+  const path = await findUp('.emb.yml');
+
+  if (!path) {
+    throw new Error('Could not find EMB config anywhere');
+  }
+
+  config = yaml.parse(path) as Config;
+
+  return config;
+};
+
+export const getConfig = () => {
+  if (!config) {
+    throw new Error(`Config not loaded, please use 'loadConfig' first`);
+  }
+
+  return config;
 };
