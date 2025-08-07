cbead6c290ea1fde4b99e7809cc87dd861bd459d
Louis Lambeau <louis.lambeau@enspirit.be>
2025-08-07 17:54:19 +0200

    Fix 'emb run' skeleton.

--- src/cli/commands/run/index.ts
+++ src/cli/commands/run/index.ts
@@ -2,41 +2,46 @@ import { Args, Command } from '@oclif/core';
 import fs from 'node:fs/promises';
 import path from 'node:path';
 
-const componentsList: Array<string> = [];
+import { getContext } from '../../context.js';
 
 export default class RunComponentScript extends Command {
   static args = {
     component: Args.string({
       description: 'Component name',
-      options: componentsList,
       required: true,
     }),
     script: Args.string({ description: 'NPM script to run', required: true }),
   };
   static description = "Run an npm script from a component's package.json";
-  static strict = false;
+  static strict = true;
 
   async run() {
     const { args } = await this.parse(RunComponentScript);
-    const { component, script } = args;
+    const { monorepo } = getContext();
 
-    const componentPath = path.resolve(component);
-    const pkgPath = path.join(componentPath, 'package.json');
+    const component = monorepo.component(args.component);
+    const pkgPath = component.join('package.json');
 
     try {
       const pkgRaw = await fs.readFile(pkgPath, 'utf8');
       const pkg = JSON.parse(pkgRaw);
 
-      if (!pkg.scripts?.[script]) {
-        this.error(`Script "${script}" not found in ${component}/package.json`);
+      if (!pkg.scripts?.[args.script]) {
+        this.error(
+          `Script "${args.script}" not found in ${component.name}/package.json`,
+        );
       }
 
-      this.log(`▶ Running "${script}" in ${component}`);
+      this.log(`▶ Running "${args.script}" in ${component.name}`);
     } catch (error) {
       if (error instanceof Error) {
-        this.error(`Failed to run ${component}:${script}\n${error.message}`);
+        this.error(
+          `Failed to run ${component.name}:${args.script}\n${error.message}`,
+        );
       } else {
-        this.error(`Failed to run ${component}:${script}\n${error as string}`);
+        this.error(
+          `Failed to run ${component.name}:${args.script}\n${error as string}`,
+        );
       }
     }
   }
