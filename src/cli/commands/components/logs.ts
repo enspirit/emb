0b40b6575792f19fab596c2c426655dc992d4846
Louis Lambeau <louis.lambeau@enspirit.be>
2025-08-14 01:16:40 +0200

    emb logs <cmp> / emp components logs <cmp>

---
+++ src/cli/commands/components/logs.ts
@@ -0,0 +1,76 @@
+import { Args, Flags } from '@oclif/core';
+
+import { FlavoredCommand, getContext } from '@/cli';
+import { ListContainersOperation } from '@/docker/index.js';
+
+export default class ComponentsLogs extends FlavoredCommand<
+  typeof ComponentsLogs
+> {
+  static aliases: string[] = ['logs'];
+  static description = 'Get components logs.';
+  static enableJsonFlag = false;
+  static examples = ['<%= config.bin %> <%= command.id %>'];
+  static flags = {
+    follow: Flags.boolean({
+      name: 'follow',
+      char: 'f',
+      description: 'Follow log output',
+      default: false,
+    }),
+  };
+  static args = {
+    component: Args.string({
+      name: 'component',
+      description: 'The component you want to see the logs of',
+      required: true,
+    }),
+  };
+
+  public async run(): Promise<void> {
+    const { flags, args } = await this.parse(ComponentsLogs);
+    const { monorepo, docker } = await getContext();
+
+    const component = monorepo.component(args.component);
+
+    const containers = await monorepo.run(new ListContainersOperation(), {
+      filters: {
+        label: [
+          `emb/project=${monorepo.name}`,
+          `emb/component=${component.name}`,
+          `emb/flavor=${monorepo.currentFlavor}`,
+        ],
+      },
+    });
+
+    if (containers.length === 0) {
+      return this.error(
+        `No container found for component \`${component.name}\``,
+      );
+    }
+
+    if (containers.length > 1) {
+      return this.error(
+        `More than one container found for component \`${component.name}\``,
+      );
+    }
+
+    const container = await docker.getContainer(containers[0].Id);
+
+    if (flags.follow) {
+      const stream = await container.logs({
+        follow: true,
+        stderr: true,
+        stdout: true,
+      });
+
+      stream.pipe(process.stdout);
+    } else {
+      const res = await container.logs({
+        stderr: true,
+        stdout: true,
+      });
+
+      this.log(res.toString());
+    }
+  }
+}
